55fae95e42c236173a7907012e8a49613fc8cf27
Lakshmi-s-rao-28 <lakshmisrao20@gmail.com>
2021-05-14 18:14:28 +0530

    #57 issue- cloanable not added in model class - FreeBusyDateModel
    added cloanable, setters and getters for time and tzid

--- src/main/java/org/biacode/jcronofy/api/model/FreeBusyDateModel.java
+++ src/main/java/org/biacode/jcronofy/api/model/FreeBusyDateModel.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class FreeBusyDateModel implements Serializable {
+public class FreeBusyDateModel implements Serializable , Cloneable {
+
+    private static final long serialVersionUID = 1L;
 
     @JsonProperty("time")
     private String time;
@@ -15,15 +17,28 @@ public class FreeBusyDateModel implements Serializable {
     private String tzid;
 
     private FreeBusyDateModel(String time, String tzid) {
-        this.time = time;
-        this.tzid = tzid;
+	this.time = time;
+	this.tzid = tzid;
     }
 
     public static FreeBusyDateModel of(String time) {
-        return new FreeBusyDateModel(time, null);
+	return new FreeBusyDateModel(time, null);
     }
 
     public static FreeBusyDateModel of(String time, String tzid) {
-        return new FreeBusyDateModel(time, tzid);
+	return new FreeBusyDateModel(time, tzid);
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+	return super.clone();
+    }
+
+    public String getTime() {
+	return time;
+    }
+
+    public String getTzid() {
+	return tzid;
     }
 }
+
